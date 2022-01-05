73750238ff3d9b5f6f8a69601da793c3eee77437
Kelsey Mailau <kelseymailau@gmail.com>
2022-01-04 21:58:55 -0800

    create prompt questions to check that it works

--- index.js
+++ index.js
@@ -1,16 +1,116 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      name: 'username',
+      message: 'What is your GitHub username?',
+      type: 'input'
+  },
+  {
+      name: 'email',
+      message: 'What is your email?',
+      type: 'input'
+  },
+  {
+      name: 'title',
+      message: 'What would you like to title your README.md?',
+      type: 'input'
+    },
+    {
+        name: 'description',
+        message: 'Describe your application.',
+        type: 'input'
+    },
+    {
+        name: 'installation',
+        message: 'What are the installation instructions for your application?',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'What is the usage information?',
+        type: 'input'
+    },
+    {
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+        type: 'input'
+    },
+    {
+        name: 'tests',
+        message: 'What are the test instructions?',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'What kind of license would you like to include?',
+        type: 'list',
+        choices: ['MIT', 'heiilo'] 
+    }
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        name: "username",
-        message: "What is your GitHub username?"
+        name: 'username',
+        message: 'What is your GitHub username?',
+        type: 'input'
     },
     {
-
+        name: 'email',
+        message: 'What is your email?',
+        type: 'input'
+    },
+    {
+        name: 'title',
+        message: 'What would you like to title your README.md file?',
+        type: 'input'
+    },
+    {
+        name: 'description',
+        message: 'Describe your application.',
+        type: 'input'
+    },
+    {
+        name: 'installation',
+        message: 'What are the installation instructions for your application?',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'What is the usage information?',
+        type: 'input'
+    },
+    {
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+        type: 'input'
+    },
+    {
+        name: 'tests',
+        message: 'What are the test instructions?',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'What kind of license would you like to include?',
+        type: 'list',
+        choices: ['MIT', 'heiilo'] 
     }
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
