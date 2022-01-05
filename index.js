c7d30d35cae0d48ffadeb4737edb56693e31bf4e
Kelsey Mailau <kelseymailau@gmail.com>
2022-01-04 21:19:26 -0800

    include inquirer in index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
